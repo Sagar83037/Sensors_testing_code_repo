a5ec15d70b64f96f8d9ddc29b672968923e24730
Sagar83037 <sagarsrivastava171@gmail.com>
2026-03-15 01:27:14 +0530

    Update LDR.ino

--- LDR(Light dependent Resistor)_sensor/LDR.ino
+++ LDR(Light dependent Resistor)_sensor/LDR.ino
@@ -1,1 +1,19 @@
+int l = A0;
+void setup (){
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+}
+void loop(){
+  int ldr = analogRead(l);
 
+  Serial.println(ldr);
+  if (ldr<10){
+    digitalWrite(13,HIGH);
+
+  }
+  else{
+    digitalWrite(13,LOW);
+  }
+  delay(1000);
+
+}
